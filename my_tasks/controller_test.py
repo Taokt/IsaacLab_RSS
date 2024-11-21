db8d3ed851942e797ac69d169e7c17f0b6ccdd7d
JialongLi <ji0341li-s@student.lu.se>
2024-11-21 17:14:50 +0100

    add kuka config

--- my_tasks/controller_test.py
+++ my_tasks/controller_test.py
@@ -53,7 +53,53 @@ from omni.isaac.lab.utils.math import subtract_frame_transforms
 ##
 # Pre-defined configs
 ##
-from omni.isaac.lab_assets import FRANKA_PANDA_HIGH_PD_CFG, UR10_CFG, KUKA_CFG # isort:skip
+# from omni.isaac.lab_assets import FRANKA_PANDA_HIGH_PD_CFG, UR10_CFG, KUKA_CFG # isort:skip
+
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.actuators import ImplicitActuatorCfg
+from omni.isaac.lab.assets.articulation import ArticulationCfg
+from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+KUKA_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path = "/home/ji0341li/CS_Project/IsaacLab_RSS/my_tasks/iiwa7.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "A1": 0.0,
+            "A2": -0.569,
+            "A3": 0.0,
+            "A4": -2.810,
+            "A5": 0.0,
+            "A6": 3.037,
+            "A7": 0.741,
+            "hande_joint_finger": 0.0,
+            "robotiq_hande_base_to_hande_right_finger": 0.0,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit=100.0,
+            effort_limit=100.0,
+            stiffness=1000000.0,
+            damping=40.0,
+        ),
+    },
+)
 
 
 @configclass
@@ -177,7 +223,7 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
 
         # apply actions
         # joint_pos_des[:, 0:9] = torch.tensor([[0,0,0,0,0,0,0,1.0, 0.025]], device='cuda:0')
-        joint_pos_des[:, 0:7] = torch.tensor([[0.0, 0.0, 0.0, 0.0, 0.0, 3.14, 0.78]], device='cuda:0')
+        # joint_pos_des[:, 0:7] = torch.tensor([[0.0, 0.0, 0.0, 0.0, 0.0, 3.14, 0.78]], device='cuda:0')
         # print('current position: ',joint_pos)
         # print('angle output: ',joint_pos_des)
         robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
--- my_tasks/my_kuka_controller.py
+++ my_tasks/my_kuka_controller.py
@@ -59,7 +59,54 @@ import numpy as np
 ##
 # Pre-defined configs
 ##
-from omni.isaac.lab_assets import FRANKA_PANDA_CFG, UR10_CFG, KUKA_CFG # isort:skip
+# from omni.isaac.lab_assets import FRANKA_PANDA_CFG, UR10_CFG, KUKA_CFG # isort:skip
+
+import omni.isaac.lab.sim as sim_utils
+from omni.isaac.lab.actuators import ImplicitActuatorCfg
+from omni.isaac.lab.assets.articulation import ArticulationCfg
+from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
+
+##
+# Configuration
+##
+
+KUKA_CFG = ArticulationCfg(
+    spawn=sim_utils.UsdFileCfg(
+        usd_path = "/home/ji0341li/CS_Project/IsaacLab_RSS/my_tasks/iiwa7.usd",
+        activate_contact_sensors=True,
+        rigid_props=sim_utils.RigidBodyPropertiesCfg(
+            disable_gravity=False,
+            max_depenetration_velocity=5.0,
+        ),
+        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
+            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+        ),
+        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
+    ),
+    init_state=ArticulationCfg.InitialStateCfg(
+        joint_pos={
+            "A1": 0.0,
+            "A2": -0.569,
+            "A3": 0.0,
+            "A4": -2.810,
+            "A5": 0.0,
+            "A6": 3.037,
+            "A7": 0.741,
+            "hande_joint_finger": 0.0,
+            "robotiq_hande_base_to_hande_right_finger": 0.0,
+        },
+    ),
+    actuators={
+        "arm": ImplicitActuatorCfg(
+            joint_names_expr=[".*"],
+            velocity_limit=100.0,
+            effort_limit=100.0,
+            stiffness=1000000.0,
+            damping=40.0,
+        ),
+    },
+)
+
 
 
 @configclass
