9413581ff2c0367e28e9b906fd39bf1467281531
DexterZzz <DexterZzz1010@gmail.com>
2024-11-28 01:00:36 +0100

    changed the usd model

--- my_tasks/dual_arm_kuka.py
+++ my_tasks/dual_arm_kuka.py
@@ -12,7 +12,7 @@ PhysX. This helps perform parallelized computation of the inverse kinematics.
 .. code-block:: bash
 
     # Usage
-    ./isaaclab.sh -p source/standalone/tutorials/05_controllers/ik_control.py
+    ./isaaclab.sh -p my_tasks/dual_arm_kuka.py --robot kuka
 
 """
 
@@ -23,9 +23,13 @@ import argparse
 from omni.isaac.lab.app import AppLauncher
 
 # add argparse arguments
-parser = argparse.ArgumentParser(description="Tutorial on using the differential IK controller.")
-parser.add_argument("--robot", type=str, default="franka_panda", help="Name of the robot.")
-parser.add_argument("--num_envs", type=int, default=1, help="Number of environments to spawn.")
+parser = argparse.ArgumentParser(
+    description="Tutorial on using the differential IK controller."
+)
+parser.add_argument("--robot", type=str, default="kuka", help="Name of the robot.")
+parser.add_argument(
+    "--num_envs", type=int, default=1, help="Number of environments to spawn."
+)
 # append AppLauncher cli args
 AppLauncher.add_app_launcher_args(parser)
 # parse the arguments
@@ -41,7 +45,10 @@ import torch
 
 import omni.isaac.lab.sim as sim_utils
 from omni.isaac.lab.assets import AssetBaseCfg
-from omni.isaac.lab.controllers import DifferentialIKController, DifferentialIKControllerCfg
+from omni.isaac.lab.controllers import (
+    DifferentialIKController,
+    DifferentialIKControllerCfg,
+)
 from omni.isaac.lab.managers import SceneEntityCfg
 from omni.isaac.lab.markers import VisualizationMarkers
 from omni.isaac.lab.markers.config import FRAME_MARKER_CFG
@@ -64,75 +71,39 @@ from omni.isaac.lab.utils.assets import ISAACLAB_NUCLEUS_DIR
 # Configuration
 ##
 
-TRASH_CAN_USD_PATH = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/my_can.usd"  
+TRASH_CAN_USD_PATH = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/my_can.usd"
 
 KUKA_CFG = ArticulationCfg(
     spawn=sim_utils.UsdFileCfg(
-        usd_path = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/iiwa7.usd",
-        # usd_path = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/dual_arm.usd",
+        usd_path="/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/KUKA_Dual_Arm_LTH.usd",
         activate_contact_sensors=True,
         rigid_props=sim_utils.RigidBodyPropertiesCfg(
             disable_gravity=False,
             max_depenetration_velocity=5.0,
         ),
         articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
+            enabled_self_collisions=True,
+            solver_position_iteration_count=8,
+            solver_velocity_iteration_count=0,
         ),
         # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
     ),
     init_state=ArticulationCfg.InitialStateCfg(
-        pos=(-1.0, 0.0, 0.0),  # 左侧机械臂基座位置 (x, y, z)
-        rot=(0.0, 0.0, 0.0, 1.0),  # 左侧机械臂基座方向 (四元数: w, x, y, z)
         joint_pos={
-            "A1": 0.0,
-            "A2": -0.569,
-            "A3": 0.0,
-            "A4": -2.810,
-            "A5": 0.0,
-            "A6": 3.037,
-            "A7": 0.741,
-            "hande_joint_finger": 0.0,
-            "robotiq_hande_base_to_hande_right_finger": 0.0,
-        },
-    ),
-    actuators={
-        "arm": ImplicitActuatorCfg(
-            joint_names_expr=[".*"],
-            velocity_limit=100.0,
-            effort_limit=100.0,
-            stiffness=1000000.0,
-            damping=40.0,
-        ),
-    },
-)
-
-KUKA_CFG_2 = ArticulationCfg(
-    spawn=sim_utils.UsdFileCfg(
-        usd_path = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/iiwa7.usd",
-        # usd_path = "/home/zyf/CS_project/3D-Diffusion-Policy-LTH/third_party/IsaacLab_RSS/my_tasks/kuka_with_can.usd",
-        activate_contact_sensors=True,
-        rigid_props=sim_utils.RigidBodyPropertiesCfg(
-            disable_gravity=False,
-            max_depenetration_velocity=5.0,
-        ),
-        articulation_props=sim_utils.ArticulationRootPropertiesCfg(
-            enabled_self_collisions=True, solver_position_iteration_count=8, solver_velocity_iteration_count=0
-        ),
-        # collision_props=sim_utils.CollisionPropertiesCfg(contact_offset=0.005, rest_offset=0.0),
-    ),
-    init_state=ArticulationCfg.InitialStateCfg(
-        pos=(1.0, 0.0, 0.0),  # 左侧机械臂基座位置 (x, y, z)
-        rot=(0.0, 0.0, 0.0, 1.0),  # 左侧机械臂基座方向 (四元数: w, x, y, z)
-        joint_pos={
-            "A1": 0.0,
-            "A2": -0.569,
-            "A3": 0.0,
-            "A4": -2.810,
-            "A5": 0.0,
-            "A6": 3.037,
-            "A7": 0.741,
-            "hande_joint_finger": 0.0,
-            "robotiq_hande_base_to_hande_right_finger": 0.0,
+            "left_iiwa_joint_1": 0.0,
+            "left_iiwa_joint_2": 0.0,
+            "left_iiwa_joint_3": 0.0,
+            "left_iiwa_joint_4": 0.0,
+            "left_iiwa_joint_5": 0.0,
+            "left_iiwa_joint_6": 0.0,
+            "left_iiwa_joint_7": 0.0,
+            "right_iiwa_joint_1": 0.0,
+            "right_iiwa_joint_2": 0.0,
+            "right_iiwa_joint_3": 0.0,
+            "right_iiwa_joint_4": 0.0,
+            "right_iiwa_joint_5": 0.0,
+            "right_iiwa_joint_6": 0.0,
+            "right_iiwa_joint_7": 0.0,
         },
     ),
     actuators={
@@ -160,24 +131,27 @@ class TableTopSceneCfg(InteractiveSceneCfg):
 
     # lights
     dome_light = AssetBaseCfg(
-        prim_path="/World/Light", spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75))
+        prim_path="/World/Light",
+        spawn=sim_utils.DomeLightCfg(intensity=3000.0, color=(0.75, 0.75, 0.75)),
     )
 
     # mount
     table = AssetBaseCfg(
         prim_path="{ENV_REGEX_NS}/Table",
         spawn=sim_utils.UsdFileCfg(
-            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd", scale=(2.0, 2.0, 2.0)
+            usd_path=f"{ISAAC_NUCLEUS_DIR}/Props/Mounts/Stand/stand_instanceable.usd",
+            scale=(2.0, 2.0, 2.0),
         ),
     )
 
     can = AssetBaseCfg(
         prim_path="/World/TrashBin",
         spawn=sim_utils.UsdFileCfg(
-            usd_path=TRASH_CAN_USD_PATH, scale=(0.5, 0.5, 0.5),
+            usd_path=TRASH_CAN_USD_PATH,
+            scale=(0.5, 0.5, 0.5),
         ),
         init_state=AssetBaseCfg.InitialStateCfg(
-        pos=(1.0, 2.0, 0.5)  # 设置垃圾桶的位置 (x, y, z)
+            pos=(1.0, 2.0, 0.5)  # 设置垃圾桶的位置 (x, y, z)
         ),
     )
 
@@ -188,10 +162,11 @@ class TableTopSceneCfg(InteractiveSceneCfg):
         robot = UR10_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
     elif args_cli.robot == "kuka":
         robot = KUKA_CFG.replace(prim_path="{ENV_REGEX_NS}/Robot")
-        robot_2 = KUKA_CFG_2.replace(prim_path="{ENV_REGEX_NS}/Robot_2")
-               
+
     else:
-        raise ValueError(f"Robot {args_cli.robot} is not supported. Valid: franka_panda, ur10")
+        raise ValueError(
+            f"Robot {args_cli.robot} is not supported. Valid: franka_panda, ur10"
+        )
 
 
 def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
@@ -201,65 +176,92 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
     robot = scene["robot"]
 
     # Create controller
-    diff_ik_cfg = DifferentialIKControllerCfg(command_type="pose", use_relative_mode=False, ik_method="dls")
-    diff_ik_controller = DifferentialIKController(diff_ik_cfg, num_envs=scene.num_envs, device=sim.device)
+    diff_ik_cfg = DifferentialIKControllerCfg(
+        command_type="pose", use_relative_mode=False, ik_method="dls"
+    )
+    diff_ik_controller = DifferentialIKController(
+        diff_ik_cfg, num_envs=scene.num_envs, device=sim.device
+    )
 
     # Markers
     frame_marker_cfg = FRAME_MARKER_CFG.copy()
     frame_marker_cfg.markers["frame"].scale = (0.1, 0.1, 0.1)
-    ee_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/ee_current"))
-    goal_marker = VisualizationMarkers(frame_marker_cfg.replace(prim_path="/Visuals/ee_goal"))
+    ee_marker = VisualizationMarkers(
+        frame_marker_cfg.replace(prim_path="/Visuals/ee_current")
+    )
+    goal_marker = VisualizationMarkers(
+        frame_marker_cfg.replace(prim_path="/Visuals/ee_goal")
+    )
 
     # Define goals for the arm
     ee_goals = [
-        [-0.5, 0.5, 0.7, 0.707, 0, 0.707, 0],
-        [-0.5, -0.4, 0.6, 0.707, 0.707, 0.0, 0.0],
-        [-0.5, 0, 0.5, 0.0, 1.0, 0.0, 0.0],
+        [-0.5, -1.0, 0.5, 0.707, 0, 0.707, 0],
+        [-1.0, -0.8, 0.4, 0.707, 0.707, 0.0, 0.0],
+        [-1.0, -0.6, 0.3, 0.0, 1.0, 0.0, 0.0],
     ]
     ee_goals = torch.tensor(ee_goals, device=sim.device)
     # Track the given command
     current_goal_idx = 0
     # Create buffers to store actions
-    ik_commands = torch.zeros(scene.num_envs, diff_ik_controller.action_dim, device=robot.device)
+    ik_commands = torch.zeros(
+        scene.num_envs, diff_ik_controller.action_dim, device=robot.device
+    )
     ik_commands[:] = ee_goals[current_goal_idx]
 
     # Specify robot-specific parameters
     if args_cli.robot == "franka_panda":
-        robot_entity_cfg = SceneEntityCfg("robot", joint_names=["panda_joint.*"], body_names=["panda_hand"])
+        robot_entity_cfg = SceneEntityCfg(
+            "robot", joint_names=["panda_joint.*"], body_names=["panda_hand"]
+        )
     elif args_cli.robot == "ur10":
-        robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*"], body_names=["ee_link"])
+        robot_entity_cfg = SceneEntityCfg(
+            "robot", joint_names=[".*"], body_names=["ee_link"]
+        )
     elif args_cli.robot == "kuka":
-        robot_entity_cfg = SceneEntityCfg("robot", joint_names=[".*"], body_names=["hande_robotiq_hande_base_link"])
+        robot_entity_cfg_left = SceneEntityCfg(
+            "robot",
+            joint_names=["left_iiwa_joint.*"],
+            body_names=["left_iiwa_link_ee"],
+        )
+        robot_entity_cfg_right = SceneEntityCfg(
+            "robot",
+            joint_names=["right_iiwa_joint.*"],
+            body_names=["right_iiwa_link_ee"],
+        )
     else:
-        raise ValueError(f"Robot {args_cli.robot} is not supported. Valid: franka_panda, ur10")
+        raise ValueError(
+            f"Robot {args_cli.robot} is not supported. Valid: franka_panda, ur10"
+        )
     # Resolving the scene entities
-    robot_entity_cfg.resolve(scene)
+    robot_entity_cfg_left.resolve(scene)
     # Obtain the frame index of the end-effector
     # For a fixed base robot, the frame index is one less than the body index. This is because
     # the root body is not included in the returned Jacobians.
     if robot.is_fixed_base:
-        ee_jacobi_idx = robot_entity_cfg.body_ids[0] - 1
+        ee_jacobi_idx = robot_entity_cfg_left.body_ids[0] - 1
     else:
-        ee_jacobi_idx = robot_entity_cfg.body_ids[0]
+        ee_jacobi_idx = robot_entity_cfg_left.body_ids[0]
 
+    print(robot_entity_cfg_left.joint_names)
+    print(robot_entity_cfg_left.body_names)
     # Define simulation stepping
     sim_dt = sim.get_physics_dt()
     count = 0
     # Simulation loop
     while simulation_app.is_running():
         # reset
-        if count % 300 == 0:
+        if count % 500 == 0:
             # reset time
             count = 0
             # reset joint state
             joint_pos = robot.data.default_joint_pos.clone()
-            print(joint_pos)
+            # print(joint_pos)
             joint_vel = robot.data.default_joint_vel.clone()
             robot.write_joint_state_to_sim(joint_pos, joint_vel)
             robot.reset()
             # reset actions
             ik_commands[:] = ee_goals[current_goal_idx]
-            joint_pos_des = joint_pos[:, robot_entity_cfg.joint_ids].clone()
+            joint_pos_des = joint_pos[:, robot_entity_cfg_left.joint_ids].clone()
             # reset controller
             diff_ik_controller.reset()
             diff_ik_controller.set_command(ik_commands)
@@ -267,23 +269,31 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
             current_goal_idx = (current_goal_idx + 1) % len(ee_goals)
         else:
             # obtain quantities from simulation
-            jacobian = robot.root_physx_view.get_jacobians()[:, ee_jacobi_idx, :, robot_entity_cfg.joint_ids]
-            ee_pose_w = robot.data.body_state_w[:, robot_entity_cfg.body_ids[0], 0:7]
+            jacobian = robot.root_physx_view.get_jacobians()[
+                :, ee_jacobi_idx, :, robot_entity_cfg_left.joint_ids
+            ]
+            ee_pose_w = robot.data.body_state_w[
+                :, robot_entity_cfg_left.body_ids[0], 0:7
+            ]
             root_pose_w = robot.data.root_state_w[:, 0:7]
-            joint_pos = robot.data.joint_pos[:, robot_entity_cfg.joint_ids]
+            joint_pos = robot.data.joint_pos[:, robot_entity_cfg_left.joint_ids]
             # compute frame in root frame
             ee_pos_b, ee_quat_b = subtract_frame_transforms(
-                root_pose_w[:, 0:3], root_pose_w[:, 3:7], ee_pose_w[:, 0:3], ee_pose_w[:, 3:7]
+                root_pose_w[:, 0:3],
+                root_pose_w[:, 3:7],
+                ee_pose_w[:, 0:3],
+                ee_pose_w[:, 3:7],
             )
             # compute the joint commands
-            joint_pos_des = diff_ik_controller.compute(ee_pos_b, ee_quat_b, jacobian, joint_pos)
+            joint_pos_des = diff_ik_controller.compute(
+                ee_pos_b, ee_quat_b, jacobian, joint_pos
+            )
+            # print(f"set joint pos:", joint_pos_des)
 
         # apply actions
-        # joint_pos_des[:, 0:9] = torch.tensor([[0,0,0,0,0,0,0,1.0, 0.025]], device='cuda:0')
-        # joint_pos_des[:, 0:7] = torch.tensor([[0.0, 0.0, 0.0, 0.0, 0.0, 3.14, 0.78]], device='cuda:0')
-        # print('current position: ',joint_pos)
-        # print('angle output: ',joint_pos_des)
-        robot.set_joint_position_target(joint_pos_des, joint_ids=robot_entity_cfg.joint_ids)
+        robot.set_joint_position_target(
+            joint_pos_des, joint_ids=robot_entity_cfg_left.joint_ids
+        )
         scene.write_data_to_sim()
         # perform step
         sim.step()
@@ -293,10 +303,12 @@ def run_simulator(sim: sim_utils.SimulationContext, scene: InteractiveScene):
         scene.update(sim_dt)
 
         # obtain quantities from simulation
-        ee_pose_w = robot.data.body_state_w[:, robot_entity_cfg.body_ids[0], 0:7]
+        ee_pose_w = robot.data.body_state_w[:, robot_entity_cfg_left.body_ids[0], 0:7]
         # update marker positions
         ee_marker.visualize(ee_pose_w[:, 0:3], ee_pose_w[:, 3:7])
-        goal_marker.visualize(ik_commands[:, 0:3] + scene.env_origins, ik_commands[:, 3:7])
+        goal_marker.visualize(
+            ik_commands[:, 0:3] + scene.env_origins, ik_commands[:, 3:7]
+        )
 
 
 def main():
